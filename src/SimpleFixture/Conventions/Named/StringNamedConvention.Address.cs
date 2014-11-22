a80e5e5eac8c9368457ed2adb5785ea2645214a9
ipjohnson <ipjohnson123@yahoo.com>
2014-11-22 12:01:42 -0700

    Adding file

---
+++ src/SimpleFixture/Conventions/Named/StringNamedConvention.Address.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SimpleFixture.Impl;
+
+namespace SimpleFixture.Conventions.Named
+{
+    public partial class StringNamedConvention
+    {
+        protected virtual string PostalCodeConvention(DataRequest request)
+        {
+            return _dataGenerator.NextString(StringType.Numeric,5,5);
+        }
+
+        protected virtual string AddressLineOneConvention(DataRequest request)
+        {
+            return null;
+        }
+
+        protected virtual string AddressLineTwoConvention(DataRequest request)
+        {
+            return null;
+        }
+
+        protected virtual string CityConvention(DataRequest request)
+        {
+            return null;
+        }
+
+        protected virtual string StateConvention(DataRequest request)
+        {
+            return null;
+        }
+
+        protected virtual string CountryConvention(DataRequest request)
+        {
+            return null;
+        }
+
+
+    }
+}
