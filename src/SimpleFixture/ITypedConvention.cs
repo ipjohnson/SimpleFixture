93cf40d400eda6d9faffaef7391f5851365261d9
ipjohnson <ipjohnson123@yahoo.com>
2014-12-20 15:36:52 -0700

    Adding documentation

--- src/SimpleFixture/ITypedConvention.cs
+++ src/SimpleFixture/ITypedConvention.cs
@@ -6,8 +6,14 @@ using System.Threading.Tasks;
 
 namespace SimpleFixture
 {
+    /// <summary>
+    /// Conventions implementing this interface support a specific set of types
+    /// </summary>
     public interface ITypedConvention : IConvention
     {
+        /// <summary>
+        /// Types the convention supports
+        /// </summary>
         IEnumerable<Type> SupportedTypes { get; }
     }
 }
