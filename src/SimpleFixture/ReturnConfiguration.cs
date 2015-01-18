fc4778189ee0e7d84aab0f3fec69b250804a9da2
ipjohnson <ipjohnson123@yahoo.com>
2015-01-18 12:22:55 -0700

    Updating documentation

--- src/SimpleFixture/ReturnConfiguration.cs
+++ src/SimpleFixture/ReturnConfiguration.cs
@@ -8,15 +8,28 @@ using SimpleFixture.Conventions;
 
 namespace SimpleFixture
 {
+    /// <summary>
+    /// Class used to configure when a return value should be used
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     public class ReturnConfiguration<T>
     {
-        private FilteredConvention<T> _convention;
+        private readonly FilteredConvention<T> _convention;
 
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        /// <param name="convention">filter convention</param>
         public ReturnConfiguration(FilteredConvention<T> convention)
         {
             _convention = convention;
         }
 
+        /// <summary>
+        /// Use return value for specific parent types
+        /// </summary>
+        /// <typeparam name="TValue">filter type</typeparam>
+        /// <returns>return configuration</returns>
         public ReturnConfiguration<T> For<TValue>()
         {
             _convention.AddFilter(r =>
@@ -34,6 +47,11 @@ namespace SimpleFixture
             return this;
         }
 
+        /// <summary>
+        /// Filter based on name (Parameter name or Property name)
+        /// </summary>
+        /// <param name="name">name to match exactly</param>
+        /// <returns>return configuration</returns>
         public ReturnConfiguration<T> WhenNamed(string name)
         {
             _convention.AddFilter(r => r.RequestName.ToLowerInvariant() == name.ToLowerInvariant());
@@ -41,6 +59,11 @@ namespace SimpleFixture
             return this;
         }
 
+        /// <summary>
+        /// Filter based on name function (Parameter name or Property name)
+        /// </summary>
+        /// <param name="namedFunc">name func used to filter</param>
+        /// <returns>return configuration</returns>
         public ReturnConfiguration<T> WhenNamed(Func<string, bool> namedFunc)
         {
             _convention.AddFilter(r => namedFunc(r.RequestName));
@@ -48,6 +71,11 @@ namespace SimpleFixture
             return this;
         }
 
+        /// <summary>
+        /// Filter based on data request
+        /// </summary>
+        /// <param name="matchingFunc">filter method</param>
+        /// <returns>return configuration</returns>
         public ReturnConfiguration<T> WhenMatching(Func<DataRequest, bool> matchingFunc)
         {
             _convention.AddFilter(matchingFunc);
