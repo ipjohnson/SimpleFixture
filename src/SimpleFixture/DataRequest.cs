0697360e17f4dfe344b92ac0cf036548f59eefa8
ipjohnson <ipjohnson123@yahoo.com>
2015-01-18 12:12:25 -0700

    Updating documentation

--- src/SimpleFixture/DataRequest.cs
+++ src/SimpleFixture/DataRequest.cs
@@ -6,8 +6,16 @@ using System.Threading.Tasks;
 
 namespace SimpleFixture
 {
+    /// <summary>
+    /// Object representing a data request
+    /// </summary>
     public class DataRequest
     {
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        /// <param name="parentRequest">parent request</param>
+        /// <param name="requestedType">new type to create</param>
         public DataRequest(DataRequest parentRequest, Type requestedType) : this(parentRequest,
                                                                                  parentRequest.Fixture,
                                                                                  requestedType,
@@ -19,6 +27,16 @@ namespace SimpleFixture
             
         }
 
+        /// <summary>
+        /// Date request constructor
+        /// </summary>
+        /// <param name="parentRequest">parent request</param>
+        /// <param name="fixture">fixture this request is associated with</param>
+        /// <param name="requestedType">type being requested</param>
+        /// <param name="requestName">request name for this request</param>
+        /// <param name="populate">populate properties</param>
+        /// <param name="constraints">constraints object</param>
+        /// <param name="extraInfo">extra info (PropertyInfo or ParameterInfo)</param>
         public DataRequest(DataRequest parentRequest, Fixture fixture, Type requestedType, string requestName, bool populate, object constraints, object extraInfo)
         {
             ParentRequest = parentRequest;
@@ -29,30 +47,47 @@ namespace SimpleFixture
             Constraints = constraints;
             ExtraInfo = extraInfo;
 
-            if (ParentRequest != null)
-            {
-                RequestDepth = parentRequest.RequestDepth + 1;
-            }
-            else
-            {
-                RequestDepth = 1;
-            }
+            RequestDepth = parentRequest != null ? parentRequest.RequestDepth + 1 : 1;
         }
 
+        /// <summary>
+        /// Parent request
+        /// </summary>
         public DataRequest ParentRequest { get; private set; }
 
+        /// <summary>
+        /// Fixture this request is associated with
+        /// </summary>
         public Fixture Fixture { get; private set; }
 
+        /// <summary>
+        /// Request name
+        /// </summary>
         public string RequestName { get; private set; }
 
+        /// <summary>
+        /// Type being requested
+        /// </summary>
         public Type RequestedType { get; private set; }
 
+        /// <summary>
+        /// Populate public properties
+        /// </summary>
         public bool Populate { get; private set; }
 
+        /// <summary>
+        /// Constraints object for request
+        /// </summary>
         public object Constraints { get; private set; }
 
+        /// <summary>
+        /// Extra Info for request (PropertyInfo or ParameterInfo)
+        /// </summary>
         public object ExtraInfo { get; private set; }
 
+        /// <summary>
+        /// Request Depth
+        /// </summary>
         public int RequestDepth { get; private set; }
     }
 }
--- src/SimpleFixture/Impl/CircularReferenceHandler.cs
+++ src/SimpleFixture/Impl/CircularReferenceHandler.cs
@@ -6,13 +6,29 @@ using System.Threading.Tasks;
 
 namespace SimpleFixture.Impl
 {
+    /// <summary>
+    /// interface to handle circular references
+    /// </summary>
     public interface ICircularReferenceHandler
     {
+        /// <summary>
+        /// Handle circular reference
+        /// </summary>
+        /// <param name="request">data request that has caused the circular reference</param>
+        /// <returns>data request value</returns>
         object HandleCircularReference(DataRequest request);
     }
 
+    /// <summary>
+    /// Default implementation of ICircularReferenceHandler, throws an exception
+    /// </summary>
     public class CircularReferenceHandler : ICircularReferenceHandler
     {
+        /// <summary>
+        /// Handle circular reference
+        /// </summary>
+        /// <param name="request">data request that has caused the circular reference</param>
+        /// <returns>data request value</returns>
         public object HandleCircularReference(DataRequest request)
         {
             throw new Exception("Circular reference detected creating " + request.RequestedType.FullName);
