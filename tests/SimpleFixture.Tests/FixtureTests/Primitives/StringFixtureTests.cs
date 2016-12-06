c802335ed3ed4db17796a2028c0a73555a129f8c
ipjohnson <ipjohnson123@yahoo.com>
2016-12-05 19:31:07 -0700

    adding tests

--- tests/SimpleFixture.Tests/FixtureTests/Primitives/StringFixtureTests.cs
+++ tests/SimpleFixture.Tests/FixtureTests/Primitives/StringFixtureTests.cs
@@ -1,6 +1,7 @@
 ﻿using FluentAssertions;
 using SimpleFixture.Tests.Classes;
 using System.Linq;
+using SimpleFixture.Impl;
 using Xunit;
 
 namespace SimpleFixture.Tests.FixtureTests.Primitives
@@ -16,6 +17,26 @@ namespace SimpleFixture.Tests.FixtureTests.Primitives
             
             ranged.TestString.Length.Should().BeInRange(50, 100);
             ranged.FirstName.All(char.IsLetter).Should().BeTrue();
+        }
+
+        [Fact]
+        public void Generate_String_Alpha()
+        {
+            var fixture = new Fixture();
+
+            var stringValue = fixture.Generate<string>(constraints: new {stringType = StringType.Alpha});
+
+            Assert.True(stringValue.All(char.IsLetter));
+        }
+
+        [Fact]
+        public void Generate_String_LoremIpsum()
+        {
+            var fixture = new Fixture();
+
+            var stringValue = fixture.Generate<string>(constraints: new { stringType = StringType.LoremIpsum });
+
+            Assert.True(RandomDataGeneratorService.LoremIpsum.StartsWith(stringValue));
         }
     }
 }
---
+++ tests/SimpleFixture.Tests/FixtureTests/RandomDataGeneratorServiceTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FluentAssertions;
+using SimpleFixture.Impl;
+using Xunit;
+
+namespace SimpleFixture.Tests.FixtureTests
+{
+    public class RandomDataGeneratorServiceTests
+    {
+        public enum RandomEnum
+        {
+            Kinda,
+            Sorta,
+            Maybe,
+            Yes,
+            No
+        }
+
+        [Fact]
+        public void RandomDataGeneratorService_Generate_Decimal()
+        {
+            var service = new RandomDataGeneratorService();
+
+            service.NextDecimal();
+        }
+
+        [Fact]
+        public void RandomDataGeneratorService_Generate_Enum()
+        {
+            var service = new RandomDataGeneratorService();
+
+            var enumValue = service.NextEnum<RandomEnum>();
+        }
+
+        [Fact]
+        public void RandomDataGeneratorService_Generate_DateTime()
+        {
+            var service = new RandomDataGeneratorService();
+
+            var dateTime = service.NextDateTime();
+        }
+
+        [Fact]
+        public void RandomDataGeneratorService_Generate_Timespan()
+        {
+            var service = new RandomDataGeneratorService();
+    
+            var value = service.NextTimeSpan();
+        }
+    }
+    }
